80c0752168678285f9da515797cef30e272f3993
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-22 18:38:13 +0800

    新增RepositoryBase ,及AccountBookRepository

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Repositories/AccountBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using MoneyTemplateMVC.Models;
+using MoneyTemplateMVC.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Repositories
+{
+    public class AccountBookRepository 
+    {
+       
+    }
+}
---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Repositories/Interface/RepositoryBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Repositories.Interface
+{
+    /// <summary>
+    /// RepositoryBase
+    /// </summary>
+    public abstract class RepositoryBase
+    {
+
+        /// <summary>
+        /// Gets or sets the connection.
+        /// </summary>
+        /// <value>
+        /// The connection.
+        /// </value>
+        protected IDbConnection Connection { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RepositoryBase"/> class.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        public RepositoryBase(IDbConnection connection)
+        {
+            this.Connection = connection;
+        }
+    }
+}
