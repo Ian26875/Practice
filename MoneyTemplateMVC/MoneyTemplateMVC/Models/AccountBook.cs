77e4a0108a0b3ca6daba5c92834ef22104ee5e07
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-22 18:19:11 +0800

    新增AccountBook DB Model

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Models/AccountBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Models
+{
+    public class AccountBook
+    {
+        public Guid Id { get; set; }
+
+        public int Categoryyy { get; set; }
+
+        public int Amounttt { get; set; }
+
+        public DateTime Dateee { get; set; }
+
+        public string Remarkkk { get; set; }
+    }
+}
