13abae0a1496e5fc35f0968e8b0c2772a33a6500
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-23 09:57:26 +0800

    新增UnitOfWork 介面 並定義方法

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Repositories/Interface/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTemplateMVC.Repositories.Interface
+{
+    public interface IUnitOfWork
+    {
+        IDbTransaction Transaction { get; }
+        IDbConnection Connection { get; }
+        IDbTransaction BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
