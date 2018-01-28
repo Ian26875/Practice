33c4d34ed46c4d7428ca054d599d49ed3c1187f5
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-28 11:51:34 +0800

    新增Enum資料夾及CategoryType

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Enum/CategoryType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Enum
+{
+    /// <summary>
+    /// CategoryType
+    /// </summary>
+    public enum CategoryType
+    {
+        /// <summary>
+        /// 支出
+        /// </summary>
+        [Display(Name = "支出")]
+        Expend,
+        /// <summary>
+        /// 收入
+        /// </summary>
+        [Display(Name = "收入")]
+        Income
+    }
+}
