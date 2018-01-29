c43d7175380ada0a5fb53bb035a93f7ba0bd01fa
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-29 22:55:24 +0800

    新增欄位驗證

--- MoneyTemplateMVC/MoneyTemplateMVC/Models/ViewModels/MoneyCreateViewModel.cs
+++ MoneyTemplateMVC/MoneyTemplateMVC/Models/ViewModels/MoneyCreateViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MoneyTemplateMVC.Enum;
+using MoneyTemplateMVC.Validators;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,14 +13,15 @@ namespace MoneyTemplateMVC.Models.ViewModels
         [Display(Name = "類別")]
         public CategoryType Category { get; set; }
 
-
+        [OnlyAllowNowAfter]
         [Display(Name = "日期")]
         public DateTime CreateTime { get; set; }
 
-
+        [Range(0,int.MaxValue)]
         [Display(Name = "金額")]
         public decimal Amount { get; set; }
 
+        [StringLength(100)]
         [Display(Name = "備註")]
         public string Remark { get; set; }
 
