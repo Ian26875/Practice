ad677d206427ebe51606489c29e2b70ea42cdcdf
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-29 22:48:58 +0800

    撰寫OnlyAllowNowAfterAttribute

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Validators/OnlyAllowNowAfterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Validators
+{
+    public class OnlyAllowNowAfterAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime)
+            {
+                var dateTime = Convert.ToDateTime(value);
+                return DateTime.Now.Year <= dateTime.Year
+                    && DateTime.Now.DayOfYear <= dateTime.DayOfYear;
+            }
+            return base.IsValid(value);
+        }
+    }
+}
