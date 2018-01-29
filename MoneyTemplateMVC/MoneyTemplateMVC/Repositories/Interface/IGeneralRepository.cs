f70abe8d29a74bc09feede4cd9607659c4fa0cbb
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-29 15:20:32 +0800

    新增泛型的IGeneralRepository

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Repositories/Interface/IGeneralRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTemplateMVC.Repositories.Interface
+{
+    public interface IGeneralRepository<T> where T : class, new()
+    {
+        IList<T> GetAll();
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T Find(object keyValues);
+    }
+}
