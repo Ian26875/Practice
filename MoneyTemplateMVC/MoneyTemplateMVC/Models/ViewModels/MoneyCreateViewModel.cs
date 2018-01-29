a6cff383710163d5258abe6848c130e444e6f16b
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-29 22:27:19 +0800

    新增MoneyCreateViewModel

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Models/ViewModels/MoneyCreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MoneyTemplateMVC.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Models.ViewModels
+{
+    public class MoneyCreateViewModel
+    {
+        [Display(Name = "類別")]
+        public CategoryType Category { get; set; }
+
+
+        [Display(Name = "日期")]
+        public DateTime CreateTime { get; set; }
+
+
+        [Display(Name = "金額")]
+        public decimal Amount { get; set; }
+
+        [Display(Name = "備註")]
+        public string Remark { get; set; }
+
+    }
+}
