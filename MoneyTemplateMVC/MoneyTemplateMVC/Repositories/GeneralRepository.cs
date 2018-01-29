837260f99a37bff3d42240932f185748e529fa12
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-29 15:26:40 +0800

    新增GeneralRepository並實作IGeneralRepository

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Repositories/GeneralRepository.cs
@@ -0,0 +1,36 @@
+﻿using MoneyTemplateMVC.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Repositories
+{
+    public class GeneralRepository<T> : IGeneralRepository<T> where T : class, new()
+    {
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(object keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
