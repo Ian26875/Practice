02a197a5c899fa932e14a49804fa0b1e96775f7e
Ian_Chen <fds26875@hotmail.com.tw>
2018-01-30 15:22:08 +0800

    新增OnlyAllowNowBeforeAttribute類別並撰寫邏輯

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Validators/OnlyAllowNowBeforeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Validators
+{
+    public class OnlyAllowNowBeforeAttribute: ValidationAttribute
+    {
+        /// <summary>
+        /// Returns true if ... is valid.
+        /// </summary>
+        /// <param name="value">要驗證之物件的值。</param>
+        /// <returns>
+        /// <see langword="true" /> 指定的值是否有效。否則， <see langword="false" />。
+        /// </returns>
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime)
+            {
+                var dateTime = Convert.ToDateTime(value);
+                bool isValidate = DateTime.Compare(DateTime.Now, dateTime) >= 0;
+                return isValidate;
+            }
+            return false;
+        }
+
+    }
+}
