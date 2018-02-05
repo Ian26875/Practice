7fa78b664249d730f87eb37305d3b27148b8a79f
Ian_Chen <fds26875@hotmail.com.tw>
2018-02-05 10:09:58 +0800

    定義RSSService介面

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Services/Interface/IRSSService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTemplateMVC.Services.Interface
+{
+    /// <summary>
+    /// IRSSService
+    /// </summary>
+    public interface IRSSService
+    {
+
+        /// <summary>
+        /// Gets the feed data.
+        /// </summary>
+        /// <returns></returns>
+        SyndicationFeed GetFeedData();
+    }
+}
