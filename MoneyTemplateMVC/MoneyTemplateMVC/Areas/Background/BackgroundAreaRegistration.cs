e98844314fde559b059ccc83e826173994e5e884
Ian_Chen <fds26875@hotmail.com.tw>
2018-02-05 14:15:47 +0800

    建立Background的 Area

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Areas/Background/BackgroundAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MoneyTemplateMVC.Areas.Background
+{
+    public class BackgroundAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Background";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Background_default",
+                "Background/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
