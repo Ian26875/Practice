c68359224ccf0bcc75e16f53d1afcdf17fdea464
Ian_Chen <fds26875@hotmail.com.tw>
2018-02-05 10:05:31 +0800

    加入System.ServiceModel參考, 撰寫RssResult.cs

--- MoneyTemplateMVC/MoneyTemplateMVC/CustomResults/RssResult.cs
+++ MoneyTemplateMVC/MoneyTemplateMVC/CustomResults/RssResult.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Web;
+using System.Web.Mvc;
+using System.Xml;
 
 namespace MoneyTemplateMVC.CustomResults
 {
-    public class RssResult
+    public class RssResult : ActionResult
     {
+        private SyndicationFeed _feed;
+
+        public RssResult(SyndicationFeed feed)
+        {
+            this._feed = feed;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.ContentType = "application/rss+xml";
+            Rss20FeedFormatter formatter = new Rss20FeedFormatter(this._feed);
+            using (var writer = XmlWriter.Create(context.HttpContext.Response.Output))
+            {
+                formatter.WriteTo(writer);
+            }
+        }
     }
 }
