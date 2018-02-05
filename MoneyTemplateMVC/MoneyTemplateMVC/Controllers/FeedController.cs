565c3fcc9714bec11864e57fbde917bb028639be
Ian_Chen <fds26875@hotmail.com.tw>
2018-02-05 10:07:00 +0800

    新增FeeController

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Controllers/FeedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoneyTemplateMVC.Controllers
+{
+    public class FeedController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
