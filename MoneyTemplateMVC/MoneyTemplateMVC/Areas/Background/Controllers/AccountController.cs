a3f9fa7a0009115c95e221e29918867b156c5beb
Ian_Chen <fds26875@hotmail.com.tw>
2018-02-06 17:37:35 +0800

    建立AccountController及ViewModel資料夾及LoginViewModel

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Areas/Background/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoneyTemplateMVC.Areas.Background.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Background/Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        
+    }
+}
---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Areas/Background/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Areas.Background.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "帳號")]
+        [StringLength(16)]
+        public string Account { get; set; }
+
+        [Display(Name = "密碼")]
+        [StringLength(16)]
+        public string Password { get; set; }
+    }
+}
