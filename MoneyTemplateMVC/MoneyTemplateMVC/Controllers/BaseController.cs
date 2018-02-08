d4fc2ce231184391dba7845591b0eb97df1f08a3
Ian_Chen <fds26875@hotmail.com.tw>
2018-02-08 14:56:46 +0800

    修改權限，並當無權限顯示您尚未登入無法觀看!! 請先登入後再嘗試。，調整為List必須要為Admin才能看

--- MoneyTemplateMVC/MoneyTemplateMVC/Controllers/BaseController.cs
+++ MoneyTemplateMVC/MoneyTemplateMVC/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoneyTemplateMVC.Filter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +7,7 @@ using System.Web.Mvc;
 
 namespace MoneyTemplateMVC.Controllers
 {
-    [Authorize]
+    [AuthorizePlus]
     public abstract class BaseController : Controller
     {
        
--- MoneyTemplateMVC/MoneyTemplateMVC/Controllers/HomeController.cs
+++ MoneyTemplateMVC/MoneyTemplateMVC/Controllers/HomeController.cs
@@ -26,26 +26,27 @@ namespace MoneyTemplateMVC.Controllers
             return View();
         }
 
-       
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [AllowAnonymous]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
-
+    
         [ChildActionOnly]
         public ActionResult List()
         {
