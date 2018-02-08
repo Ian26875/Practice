af589966b28f60c2bc1fd42de83af16459947d80
Ian_Chen <fds26875@hotmail.com.tw>
2018-02-08 10:56:43 +0800

    Index 畫面ＵＩ調整，加入Identity參考及在Gloal設定AntiForgeryConfig,加入Startup.cs設定

--- MoneyTemplateMVC/MoneyTemplateMVC/Areas/Background/Controllers/AccountController.cs
+++ MoneyTemplateMVC/MoneyTemplateMVC/Areas/Background/Controllers/AccountController.cs
@@ -1,13 +1,16 @@
-﻿using MoneyTemplateMVC.Areas.Background.Models.ViewModels;
+﻿using Microsoft.AspNet.Identity;
+using MoneyTemplateMVC.Areas.Background.Models.ViewModels;
 using MoneyTemplateMVC.Services.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
 namespace MoneyTemplateMVC.Areas.Background.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private readonly IMemberService _memberService;
@@ -17,19 +20,40 @@ namespace MoneyTemplateMVC.Areas.Background.Controllers
             this._memberService = memberService;
         }
 
-
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
-
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        [AllowAnonymous]
         public ActionResult Login(LoginViewModel loginView)
         {
             if (ModelState.IsValid == false)
             {
                 return View("Index", loginView);
             }
-            return RedirectToAction("Index", "Home", new { area = "" });
+            bool isLogin = _memberService.Login(loginView.Account,loginView.Password);
+            if (isLogin)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,loginView.Account),
+                  
+                    new Claim(ClaimTypes.Role, "Admin")
+                };
+                var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+                var ctx = Request.GetOwinContext();
+                var authenticationManager = ctx.Authentication;
+                authenticationManager.SignIn(identity);
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            else
+            {
+                ModelState.AddModelError("", "請申請會員！");
+                return View("Index", loginView);
+            }
         }
 
     }
