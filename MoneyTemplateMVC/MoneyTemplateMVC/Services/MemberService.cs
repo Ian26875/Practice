b20cecda4a5dd587f14b123ecc708c947342f822
Ian_Chen <fds26875@hotmail.com.tw>
2018-02-08 09:51:29 +0800

    新增ＭemberService類別及介面

---
+++ MoneyTemplateMVC/MoneyTemplateMVC/Services/MemberService.cs
@@ -0,0 +1,16 @@
+﻿using MoneyTemplateMVC.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateMVC.Services
+{
+    public class MemberService : IMemberService
+    {
+        public bool Login(string account, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
